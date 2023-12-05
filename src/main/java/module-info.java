dfee94a55b0aca52cf9cc18f9f2301981923f2c8
Jowy77 <Iproyectz@hotmail.com>
2023-12-05 19:05:00 +0000

    emepzando interfaces

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.proyectojdbc {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.base;
     
     opens com.mycompany.proyectojdbc to javafx.fxml;
     exports com.mycompany.proyectojdbc;
